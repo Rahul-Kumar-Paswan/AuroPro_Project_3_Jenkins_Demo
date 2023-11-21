b8a134c22cbc9a66bb88fd5b3ffd4a8b09fafdcb
Rahul-Kumar-Paswan <rahulkumarpaswan941@gmail.com>
2023-11-21 14:48:30 +0530

    initial

---
+++ AuroPro_Project_3/modules/instances/outputs.tf
@@ -0,0 +1,21 @@
+output "instance_id" {
+  value = aws_instance.my_instance.id
+}
+
+output "public_ip" {
+  value = aws_instance.my_instance.public_ip
+}
+
+output "private_ip" {
+  value = aws_instance.my_instance.private_ip
+}
+
+output "my_security_group_id" {
+  value = aws_security_group.my_security_group.id
+}
+
+output "private_key" {
+  value       = aws_key_pair.ssh-key.private_key
+  sensitive   = true
+  description = "Private key for the SSH key pair"
+}
