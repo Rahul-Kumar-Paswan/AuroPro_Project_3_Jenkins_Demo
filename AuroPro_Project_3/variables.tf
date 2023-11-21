9353ea3e5e1312e9db3a43a52171f4cad7ff6cfb
Rahul-Kumar-Paswan <rahulkumarpaswan941@gmail.com>
2023-11-21 16:54:16 +0530

    added env

--- AuroPro_Project_3/variables.tf
+++ AuroPro_Project_3/variables.tf
@@ -1,5 +1,7 @@
 variable "vpc_cidr_block" {}
-variable "env_prefix" {}
+variable "env_prefix" {
+  default = "dev"
+}
 variable "public_subnet_cidr_block" {}
 variable "private_subnet_cidr_block" {}
 variable "public_subnet_availability_zone" {}
