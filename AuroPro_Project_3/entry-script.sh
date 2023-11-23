261b4b63b09c0171100d1ee8c36672d0c499fdc8
Rahul-Kumar-Paswan <rahulkumarpaswan941@gmail.com>
2023-11-23 12:43:35 +0530

    added public_ip in output.tf

--- AuroPro_Project_3/entry-script.sh
+++ AuroPro_Project_3/entry-script.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-"sudo yum update -y",
-"sudo yum install docker -y",
-"sudo service docker start",
-"sudo usermod -a -G docker ec2-user",
-"sudo chmod 666 /var/run/docker.sock",
-"sudo service docker restart",
-"sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose",
-"sudo chmod +x /usr/local/bin/docker-compose",
-"docker-compose --version"
-            
+sudo yum update -y && sudo yum install -y docker
+sudo systemctl start docker 
+sudo usermod -aG docker ec2-user
+sudo chmod 666 /var/run/docker.sock
+sudo service docker restart
+
+# install docker-compose 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
