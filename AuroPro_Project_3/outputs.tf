4772be8bc059fa28156b6d0559cfa7c8ad086d70
Rahul-Kumar-Paswan <rahulkumarpaswan941@gmail.com>
2023-11-23 14:37:31 +0530

    apply

--- AuroPro_Project_3/outputs.tf
+++ AuroPro_Project_3/outputs.tf
@@ -23,10 +23,10 @@ output "private_ip" {
 # }
 
 # output "private_key_pem" {
-#   value = module.my_instance.ssh-key.private_key_pem
+#   value = module.my_instance.ssh_key.private_key_pem
 # }
 
 output "private_key_pem" {
-  value = module.my_instance.private_key_pem
+  value = module.ssh_key.private_key_pem
 }
 
