1597c67fafb59eec7373044667f4ac20fe4169d0
Rahul-Kumar-Paswan <rahulkumarpaswan941@gmail.com>
2023-11-23 12:40:37 +0530

    added public_ip in output.tf

--- AuroPro_Project_3/outputs.tf
+++ AuroPro_Project_3/outputs.tf
@@ -9,3 +9,11 @@ output "public_subnet_id" {
 output "private_subnet_id" {
   value = module.my_vpc.private_subnet_id
 }
+
+output "public_ip" {
+  value = module.aws_instance.my_instance.public_ip
+}
+
+output "private_ip" {
+  value = module.aws_instance.my_instance.private_ip
+}
