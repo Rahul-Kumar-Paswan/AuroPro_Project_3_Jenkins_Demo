41ea385578a7472b2f86a1e326f60768df184e53
Rahul-Kumar-Paswan <rahulkumarpaswan941+auropro@gmail.com>
2023-11-24 22:17:31 +0530

    destroy

--- AuroPro_Project_3/entry-script.sh
+++ AuroPro_Project_3/entry-script.sh
@@ -6,8 +6,9 @@ sudo chmod 666 /var/run/docker.sock
 sudo service docker restart
 
 # install mysql 
-sudo yum update
-sudo yum install mysql
+sudo yum update -y
+sudo yum install mariadb-server
+sudo systemctl start mariadb
 
 # install docker-compose 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
